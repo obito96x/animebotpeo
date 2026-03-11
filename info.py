c81696a0198dee4693e9b443105017bda64245e8
obito96x <uffobitoxe@gmail.com>
2026-03-11 22:15:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,6 +21,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21352768'))
 API_HASH = environ.get('API_HASH', '5fecf44f5dd9f46410d6d835491ae4a3')
 BOT_TOKEN = environ.get('BOT_TOKEN', "8047158900:AAElwDaX8m-V7Vq6MeJqKx1O1UunnKCFJ10")
+OWNER_USERNAME = environ.get('OWNER_USERNAME', 'i_killed_my_clan')
 
 # ============================
 # Bot Settings Configuration
